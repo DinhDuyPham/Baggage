b84497cf094988c0ddac9e9790f2633e401fbf07
Dinh Duy <0366010143duy@gmail.com>
2022-12-13 21:30:51 +0700

    Add project files.

---
+++ Baggage/Controllers/CommingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baggage.Controllers
+{
+    public class CommingController : Controller
+    {
+        // GET: Comming
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Baggage/Controllers/RegissterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baggage.Controllers
+{
+    public class RegissterController : Controller
+    {
+        // GET: Regisster
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
